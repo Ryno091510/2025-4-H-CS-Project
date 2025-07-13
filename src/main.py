270816f8b69caebc6059227793610412a224751e
Ryno091510 <ryan.g.mejeur@gmail.com>
2025-07-13 13:31:11 -0400

    Fully functioning code and some more documentation, ony documentation left

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@
 # ---------------------------------------------------------------------------- #
 
 # Library imports
-from vex import *
+from vex import *                                                               # Imports everything from the vex library for the robot to use
 import math
 
 
@@ -18,13 +18,13 @@ class ClawStates():
 
 class ArmHeights(int):
     FLOOR = 10
-    LEVEL_1 = 300
-    LEVEL_2 = 475
-    LEVEL_3 = 695
+    LEVEL_1 = 255
+    LEVEL_2 = 445
+    LEVEL_3 = 690
 
 #region Subsystems
 
-class DriveSubsystem():                                                         # Defines the DriveSubsytem class
+class DriveSubsystem():
     '''
     A subsystem to control the robot's drivetrain.
     '''
@@ -39,7 +39,7 @@ class DriveSubsystem():                                                         
 
         self.gyro = Inertial(Ports.PORT18)                                      # Initializes the gyro sensor to detect the robot's heading
 
-        self.leftMotor = Motor(Ports.PORT11, False)                             # |
+        self.leftMotor = Motor(Ports.PORT12, False)                             # |
         self.backMotor = Motor(Ports.PORT19, True)                              #  \ Initializes all of the drive motors, with the first argument being the port,
         self.rightMotor = Motor(Ports.PORT20, True)                             #  / and the second determining whether or not the motor is reversed
         self.frontMotor = Motor(Ports.PORT1, False)                             # |
@@ -53,7 +53,7 @@ class DriveSubsystem():                                                         
         self.targetHeading = 0                                                  # Defines the targetHeading varaible
         self.heading = 0.0                                                      # Defines the heading variable
 
-        self.turnSpeed = int(0)                                                 # 
+        self.turnSpeed = 0                                                      # 
         self.yMoveSpeed = 0                                                     # 
         self.xMoveSpeed = 0                                                     # 
 
@@ -75,7 +75,7 @@ class DriveSubsystem():                                                         
                 self.turningToTarget = False                                    # Set turningToTarget to false
                 self.stopTurn()                                                 # Stop turning
             
-            elif abs(self.heading-self.targetHeading) < 180:                            # If within 180 degrees of target angle
+            elif abs(self.heading-self.targetHeading) < 180:                            # If the absolute of difference of the heading and target is less than 180
                 if self.heading > self.targetHeading: self.turn(activeTurnSpeed)        # If clockwise of target angle, turn normally
                 elif self.heading < self.targetHeading: self.turn(-activeTurnSpeed)     # If counterclockwise of target angle, turn reversed
 
@@ -89,9 +89,9 @@ class DriveSubsystem():                                                         
         motorSpeeds[2] = (self.turnSpeed + self.yMoveSpeed) / 2                 #   |
         motorSpeeds[3] = (-self.turnSpeed + self.xMoveSpeed) / 2                #   |
         
-        self.leftMotor.spin(FORWARD, motorSpeeds[0], VelocityUnits.PERCENT)     # Sending the target motors speeds to the motors
-        self.backMotor.spin(FORWARD, motorSpeeds[1], VelocityUnits.PERCENT)     #   |
+        self.leftMotor.spin(FORWARD, motorSpeeds[0], VelocityUnits.PERCENT)     # Sending the target motor speeds to the motors
         self.rightMotor.spin(FORWARD, motorSpeeds[2], VelocityUnits.PERCENT)    #   |
+        self.backMotor.spin(FORWARD, motorSpeeds[1], VelocityUnits.PERCENT)     #   |
         self.frontMotor.spin(FORWARD, motorSpeeds[3], VelocityUnits.PERCENT)    #   |
 
     def turn(self, speed: int):
@@ -104,11 +104,11 @@ class DriveSubsystem():                                                         
 
     def stopTurn(self):
         '''
-        Stops the robot's turn
+        Stops the robot's turn.
         ''' 
         self.turnSpeed = 0                                                      # Sets the turnSpeed variable to 0
 
-    def driveFieldOriented(self, vertSpeed, horSpeed):
+    def driveFieldOriented(self, vertSpeed: int, horSpeed: int):
         '''
         Commands the robot to drive field-oriented, which means that one direction is always forward,
         no matter which way the robot is facing.
@@ -119,29 +119,54 @@ class DriveSubsystem():                                                         
         headingRadians = math.radians(self.heading)                             # Gets the robot's current heading in radians
         sinH = math.sin(headingRadians)                                         # Gets the sine of the robot's heading
         cosH = math.cos(headingRadians)                                         # Gets the cosine of the robot's heading
-        self.yMoveSpeed = ((cosH * vertSpeed) + (sinH * horSpeed))              # Get the y movement speed from multiplication of the cosine, sine, and input variables
-        self.xMoveSpeed = (-(sinH * vertSpeed) + (cosH * horSpeed))             # Get the x movement speed from multiplication of the sine, cosine, and input variables
+        self.yMoveSpeed = ((cosH * vertSpeed) + (sinH * horSpeed))              # Sets the y movement speed from multiplication of the cosine, sine, and input variables
+        self.xMoveSpeed = (-(sinH * vertSpeed) + (cosH * horSpeed))             # Sets the x movement speed from multiplication of the sine, cosine, and input variables
     
-    def driveRobotOriented(self, vertSpeed, horSpeed):
+    def driveRobotOriented(self, vertSpeed: int, horSpeed: int):
         '''
-        Commands the robot to drive oriented to the direction it is facing
+        Commands the robot to drive oriented to the direction it is facing.
+
+        :param int vertSpeed: The speed the robot should move forward and backward
+        :param int horSpeed: The speed the robot should move left and right
         '''
-        self.yMoveSpeed = vertSpeed                                             # 
-        self.xMoveSpeed = horSpeed                                              # 
+        self.yMoveSpeed = vertSpeed                                             # Sets the y movement speed to the supplied vertical speed
+        self.xMoveSpeed = horSpeed                                              # Sets the x movement speed to the supplied horizontal speed
     
     def stopDrive(self):
-        self.yMoveSpeed = 0                                                     # 
-        self.xMoveSpeed = 0                                                     # 
+        '''
+        Stops the robot's driving.
+        '''
+        self.yMoveSpeed = 0                                                     # Sets the y movement speed to 0
+        self.xMoveSpeed = 0                                                     # Sets the x movement speed to 0
 
-    def turnToHeading(self, target):
-        self.turningToTarget = True                                             # 
-        self.targetHeading = target                                             # 
+    def turnToHeading(self, target: float):
+        '''
+        Commands the robot to face a specified heading.
+
+        :param float target
+        '''
+        self.turningToTarget = True                                             # Sets the turningToTarget variable to True
+        self.targetHeading = target                                             # Sets the target heading to the supplied target
 
     def resetHeading(self):
-        self.gyro.reset_heading()                                               # 
+        '''
+        Resets the gyro on the robot back to zero.
+        '''
+        self.gyro.reset_heading()                                               # Resets the heading on the gyro
 
 class VisionSubsystem():
+    '''
+    A subsystem for communicating with the AI Vision Sensor.
+    '''
     def __init__(self, dSubsystem: DriveSubsystem, brain: Brain):
+        '''
+        Initializes the subsystem as an object.
+
+        Parameters:
+            dSubsystem: The drive subsystem
+
+            brain: 
+        '''
         self.tags = [
             Tagdesc(0),
             Tagdesc(1),
@@ -149,6 +174,7 @@ class VisionSubsystem():
             Tagdesc(3),
             Tagdesc(4),
             Tagdesc(5)]
+        
         self.colors = [
             Colordesc(1, 71, 200, 200, 20, 0.4)
         ]
@@ -348,31 +374,30 @@ def CollectFromStack(id, dSubsystem: DriveSubsystem, vSubsystem: VisionSubsystem
     dSubsystem.stopDrive()
 
 def DropInStack(id, dSubsystem: DriveSubsystem, vSubsystem: VisionSubsystem, cSubsystem: ClawSubsystem):
-    dSubsystem.turnToHeading(vSubsystem.tagHeadings[id]); print('start turn')
-    step = 0
-    while dSubsystem.turningToTarget: wait(20, MSEC); step += 1; print('turning', step)
-    wait(250, MSEC); print('waiting 250')
+    dSubsystem.turnToHeading(vSubsystem.tagHeadings[id])
+    while dSubsystem.turningToTarget: wait(20, MSEC)
+    wait(250, MSEC)
     proximity = 100
-    if vSubsystem.towerHeight == 0: cSubsystem.setArmHeight(ArmHeights.FLOOR); proximity = 30; print('height 0')
-    elif vSubsystem.towerHeight == 1: cSubsystem.setArmHeight(ArmHeights.LEVEL_1); proximity = 25; print('height 1')
-    elif vSubsystem.towerHeight == 2: cSubsystem.setArmHeight(ArmHeights.LEVEL_2); proximity = 25; print('height 2')
-    elif vSubsystem.towerHeight == 3: cSubsystem.setArmHeight(ArmHeights.LEVEL_3); proximity = 30; print('height 3')
-    wait(50, MSEC); print('waiting 50')
-    step = 0
-    while not cSubsystem.armAtTarget: wait(20, MSEC); step += 1; print('lifting', step)
-    dSubsystem.driveRobotOriented(50, 0); print('drive front')
-    step = 0
-    while vSubsystem.tagSnapshots[id][0].width < proximity: wait(20, MSEC); step += 1; print('driving', step)
-    dSubsystem.stopDrive(); print('stop')
-    cSubsystem.openClaw(); print('open')
-    step = 0
-    while cSubsystem.currentClawState == ClawStates.CLOSED: wait(20, MSEC); step += 1; print('opening', step)
-    dSubsystem.driveRobotOriented(-50, 0); print('drive back')
-    wait(1, SECONDS); print('wait 1 sec')
-    cSubsystem.setArmHeight(ArmHeights.FLOOR); print('arm down')
-    dSubsystem.stopDrive(); print('stop')
+    if vSubsystem.towerHeight == 0: cSubsystem.setArmHeight(ArmHeights.FLOOR); proximity = 30
+    elif vSubsystem.towerHeight == 1: cSubsystem.setArmHeight(ArmHeights.LEVEL_1); proximity = 25
+    elif vSubsystem.towerHeight == 2: cSubsystem.setArmHeight(ArmHeights.LEVEL_2); proximity = 25
+    elif vSubsystem.towerHeight == 3: cSubsystem.setArmHeight(ArmHeights.LEVEL_3); proximity = 30
+    wait(50, MSEC)
+    while not cSubsystem.armAtTarget: wait(20, MSEC)
+    dSubsystem.driveRobotOriented(50, 0)
+    while vSubsystem.tagSnapshots[id][0].width < proximity:
+        if not vSubsystem.tagHeadings[id]-1.5 < dSubsystem.heading < vSubsystem.tagHeadings[id]+1.5:
+            dSubsystem.turnToHeading(vSubsystem.tagHeadings[id])
+        wait(20, MSEC)
+    dSubsystem.stopDrive()
+    cSubsystem.openClaw()
+    while cSubsystem.currentClawState == ClawStates.CLOSED: wait(20, MSEC)
+    dSubsystem.driveRobotOriented(-50, 0)
+    wait(1, SECONDS)
+    cSubsystem.setArmHeight(ArmHeights.FLOOR)
+    dSubsystem.stopDrive()
 
-def ScanForTag(toZero: bool, dSubsystem: DriveSubsystem, vSubsystem: VisionSubsystem):
+def ScanForTags(toZero: bool, dSubsystem: DriveSubsystem, vSubsystem: VisionSubsystem):
     if toZero:
         dSubsystem.turnToHeading(5)
         while dSubsystem.turningToTarget: pass
@@ -392,29 +417,28 @@ def ScanForTag(toZero: bool, dSubsystem: DriveSubsystem, vSubsystem: VisionSubsy
 #endregion Commands
 
 
-brain = Brain()
-controller = Controller()
+brain = Brain()                                                                 # defines the Vex V5 brain
+controller = Controller()                                                       # defines the Vex V5 controller
 
-driveSubsystem = DriveSubsystem(brain)
-visionSubsystem = VisionSubsystem(driveSubsystem, brain)
-clawSubsystem = ClawSubsystem(brain)
+driveSubsystem = DriveSubsystem(brain)                                          # defines the drive subsystem
+visionSubsystem = VisionSubsystem(driveSubsystem, brain)                        # defines the vision subsystem
+clawSubsystem = ClawSubsystem(brain)                                            # defines the claw subsystem
 
-
-
-controllerL1Event = controller.buttonL1.pressed(clawSubsystem.toggleClaw)
-controllerR1Event = controller.buttonR1.pressed(clawSubsystem.raiseArm)
-controllerR2Event = controller.buttonR2.pressed(clawSubsystem.lowerArm)
-
-emergencyStopEvent = controller.buttonLeft.pressed(brain.program_stop)
+emergencyStopEvent = controller.buttonLeft.pressed(brain.program_stop)          # ends the program when you hit the left arrow on the controller
+controller.buttonR1.pressed(clawSubsystem.raiseArm)
+controller.buttonR2.pressed(clawSubsystem.lowerArm)
 
 
 def periodicThread():
-    while True:
-        driveSubsystem.periodic()
-        visionSubsystem.periodic()
-        clawSubsystem.periodic()
+    '''
+    All of the code in here is used for updating the various subsystems
+    '''
+    while True:                                                                 # Loop the following code forever
+        driveSubsystem.periodic()                                               # runs the drive subsystem's periodic function 
+        visionSubsystem.periodic()                                              # runs the vision subsystem's periodic function
+        clawSubsystem.periodic()                                                # runs the claw subsystem's periodic function
         displayDebugData()
-        wait(20, MSEC)
+        wait(20, MSEC)                                                          # delays the thread twenty milliseconds so it doesn't take up all of the resources
 
 def displayDebugData():
     controller.screen.clear_screen()
@@ -426,30 +450,24 @@ def displayDebugData():
     controller.screen.print(str(clawSubsystem.armAtTarget))
 
 
-# while True:
-    
-#     if controller.buttonA.pressing():
-#         CollectFromStack(1, driveSubsystem, visionSubsystem, clawSubsystem)
-#     if controller.buttonB.pressing():
-#         DropInStack(1, driveSubsystem, visionSubsystem, clawSubsystem)
 
-#     controller.screen.clear_screen()
-#     controller.screen.set_cursor(1, 1)
-#     controller.screen.print(driveSubsystem.heading)
-#     controller.screen.next_row()
-#     controller.screen.print(visionSubsystem.towerHeight)
-#     controller.screen.next_row()
-#     controller.screen.print(visionSubsystem.tagHeadings[1])
-#     wait(50, MSEC)
+if __name__ == "__main__": 
+    wait(2, SECONDS)                                                            # Delays the program two seconds to allow the gyro to fully calibrate
 
-wait(2, SECONDS)
+    periodic = Thread(periodicThread)                                           # Initializes the periodic thread so it will run seperately from the main code
 
-periodic = Thread(periodicThread)
+    ScanForTags(False, driveSubsystem, visionSubsystem)                         # Scans for tags without turning to zero first
 
-ScanForTag(False, driveSubsystem, visionSubsystem)
+    CollectFromStack(0, driveSubsystem, visionSubsystem, clawSubsystem)         # Collects from the stack with AprilTag 0
 
-CollectFromStack(0, driveSubsystem, visionSubsystem, clawSubsystem)
+    ScanForTags(True, driveSubsystem, visionSubsystem)                          # Scans for tags at the new location, turning to zero first
 
-ScanForTag(True, driveSubsystem,visionSubsystem)
+    DropInStack(1, driveSubsystem, visionSubsystem, clawSubsystem)              # Drops the ball in the stack with AprilTag 1
 
-DropInStack(1, driveSubsystem, visionSubsystem, clawSubsystem)
+    ScanForTags(True, driveSubsystem, visionSubsystem)                          # Scans for tags at the new location, turning to zero first
+
+    CollectFromStack(2, driveSubsystem, visionSubsystem, clawSubsystem)         # Collects from the stack with AprilTag 2
+
+    ScanForTags(True, driveSubsystem, visionSubsystem)                          # Scans for tags at the new location, turning to zero first
+
+    DropInStack(3, driveSubsystem, visionSubsystem, clawSubsystem)              # Drops the ball in the stack with AprilTag 3
